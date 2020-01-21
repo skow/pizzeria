c5673c13b93698dafbf2c6fc388c293e7c0fb41f
Krzysztof Skowronski <skovron@bincore.pl>
2020-01-21 12:24:25 +0100

    Add class Booking to project

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function(){
@@ -99,6 +100,13 @@ const app = {
     });
   },
 
+  initBooking: function () {
+    const thisApp = this;
+
+    const containerWidget = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(containerWidget);
+  },
+
   init: function () {
     const thisApp = this;
     // console.log('*** App starting ***');
@@ -109,6 +117,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
